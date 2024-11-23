284cbd3bd61cd6e56a895a569ebe9e7acadb27a7
max <cyan0908@163.com>
2024-11-23 17:42:52 +0800

    feat: get meta at web app

--- internal/api-server/controller/gin/v1/web/chat_app/app/app_meta.go
+++ internal/api-server/controller/gin/v1/web/chat_app/app/app_meta.go
@@ -1,7 +1,14 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func (ac *WebAppController) AppMeta(c *gin.Context) {
 
+	c.JSON(http.StatusOK, map[string]map[string]any{
+		"tool_icons": {},
+	})
 }
